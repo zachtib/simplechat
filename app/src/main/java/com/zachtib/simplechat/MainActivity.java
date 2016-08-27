16bc66b2001f013d89b4e669b17f323203aff58c
Zach Tibbitts <zachtib@gmail.com>
2016-08-27 16:04:46 -0400

    Navigation menu w/ header populated from user data

--- app/src/main/java/com/zachtib/simplechat/MainActivity.java
+++ app/src/main/java/com/zachtib/simplechat/MainActivity.java
@@ -3,18 +3,29 @@ package com.zachtib.simplechat;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,
+        GoogleApiClient.OnConnectionFailedListener {
     private static final String TAG = "MainActivity";
     public static final String ANONYMOUS = "anonymous";
 
@@ -32,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     private ListView mDrawerList;
     private TextView mNameTextView;
     private TextView mEmailTextView;
+    private ImageView mPhotoImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +53,14 @@ public class MainActivity extends AppCompatActivity {
 
         NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
         View headerView = navView.inflateHeaderView(R.layout.nav_header_main);
+        navView.inflateMenu(R.menu.activity_main_drawer);
+        navView.setNavigationItemSelectedListener(this);
+
         mNameTextView = (TextView) headerView.findViewById(R.id.name_textview);
         mEmailTextView = (TextView) headerView.findViewById(R.id.email_textview);
+        mPhotoImageView = (ImageView) headerView.findViewById(R.id.photo_image_view);
 
         checkFirebaseAuthentication();
-
-        // greet our user
-        ((TextView) findViewById(R.id.greeter_text)).setText(String.format("Hello, %s", mUsername));
     }
 
     public void checkFirebaseAuthentication() {
@@ -64,11 +77,40 @@ public class MainActivity extends AppCompatActivity {
             mUsername = mFirebaseUser.getDisplayName();
             mNameTextView.setText(mUsername);
             if (mFirebaseUser.getEmail() != null) {
-                //mEmailTextView.setText(mFirebaseUser.getEmail());
+                mEmailTextView.setText(mFirebaseUser.getEmail());
             }
             if (mFirebaseUser.getPhotoUrl() != null) {
                 mPhotoUrl = mFirebaseUser.getPhotoUrl().toString();
+                Glide.with(this)
+                        .load(mPhotoUrl)
+                        .into(mPhotoImageView);
             }
         }
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API)
+                .build();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_sign_out:
+                mFirebaseAuth.signOut();
+                Auth.GoogleSignInApi.signOut(mGoogleApiClient);
+                mUsername = ANONYMOUS;
+                startActivity(new Intent(this, SignInActivity.class));
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
     }
 }
